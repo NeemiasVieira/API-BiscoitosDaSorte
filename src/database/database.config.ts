297c73ca131040eebded067c51ebdfe14671a82f
Neemias Vieira <neemias.vieira@live.com>
2023-08-22 15:50:45 -0300

    Correção de bugs

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize-typescript';
 import { Feedbacks } from 'src/feedbacks/feedback.model';
 import { BiscoitosDaSorte } from 'src/biscoitos-da-sorte/BiscoitoDaSorte.model';
 import { BiscoitosDoAzar } from 'src/biscoitos-do-azar/biscoitos-do-azar.model';
-import pg from 'pg'
+import pg from 'pg';
 
 export const databaseProviders = [
   {
@@ -10,16 +10,17 @@ export const databaseProviders = [
     useFactory: async () => {
       const sequelize = new Sequelize({
         dialect: 'postgres',
-        host: 'biscoitos-da-sorte.postgres.database.azure.com',
+        host: process.env.HOST,
         port: 5432, // A porta padrão do PostgreSQL
-        username: 'neemias',
-        password: 'SenhaDoBanco987@_',
+        username: process.env.USER,
+        password: process.env.PASSWORD,
         database: 'postgres',
-        dialectModule: pg,
-        dialectOptions: {
+        dialectModule: pg, //Necessário para o deploy na vercel
+        dialectOptions: { //Necessário para usar o servidor Postgre no Azure
           ssl: {      require: true, 
           }
-      }});
+      }
+    });
       sequelize.addModels([Feedbacks, BiscoitosDaSorte, BiscoitosDoAzar])
       await sequelize.sync();
       try {
